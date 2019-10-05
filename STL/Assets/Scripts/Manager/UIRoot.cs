18d5d6b3772d2095c9058af7dfa0a562c0b0b93b
SeongSeop <tjdtjq96@gmail.com>
2019-10-05 16:52:02 +0900

    타이틀 화면추가.

--- STL/Assets/Scripts/Manager/UIRoot.cs
+++ STL/Assets/Scripts/Manager/UIRoot.cs
@@ -1,13 +1,56 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+using DG.Tweening;
 
 public class UIRoot : Singleton<UIRoot>
 {
-    // Start is called before the first frame update
-    void Start()
+    public enum MenuType
     {
-        
+        Title,
+        Main,
+        Ending,
     }
-     
+
+
+    [SerializeField] private GameObject[] MenuRoots;
+    [SerializeField] private RawImage titleImage;
+    [SerializeField] private float tweenDelay;
+
+    Tween fadeTween;
+
+
+    private void Start()
+    {
+        SwitchMenu(MenuType.Title);
+    }
+
+    public void SwitchMenu(MenuType newMenu)
+    {
+        foreach(var root in MenuRoots)
+        {
+            root.gameObject.SetActive(false);
+        }
+        MenuRoots[(int)newMenu].gameObject.SetActive(true); 
+    }
+
+    public void OnClickTitleImage()
+    {
+        if (fadeTween!= null && fadeTween.IsPlaying())
+            return;
+
+        fadeTween = titleImage.DOFade(0, tweenDelay).OnComplete(() =>
+        {
+            SwitchMenu(MenuType.Main); 
+        }); 
+    }
+
+
+    public void OnClickEnding()
+    {
+
+    }
+
+
 }
