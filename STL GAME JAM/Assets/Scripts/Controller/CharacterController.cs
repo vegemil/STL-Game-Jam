ce8460f7b8db4998f9b180b686f586df64b13407
SeongSeop <tjdtjq96@gmail.com>
2019-10-05 10:14:52 +0900

    <기능> 캐릭터 베이스 추가.

---
+++ STL GAME JAM/Assets/Scripts/Controller/CharacterController.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharacterController : MonoBehaviour
+{
+    [SerializeField] private CharacterMovement movement;
+    [SerializeField] private CharacterBase character;
+
+
+
+    public void Initalize()
+    {
+
+    }
+
+
+
+}
